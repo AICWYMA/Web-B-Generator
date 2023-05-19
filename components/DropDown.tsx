ba18dfc608515204b2e26bcde6ddefed38948e71
AICWYMA <132848832+AICWYMA@users.noreply.github.com>
2023-05-19 15:05:09 -0700

    Update DropDown.tsx

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type VibeType = "Professional" | "Casual" | "Funny" | "Energetic" | "Relaxed";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let vibes: VibeType[] = ["Professional", "Casual", "Funny", "Energetic", "Relaxed"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
